e276c64ad4e7e004e1ae1e4d49982d327aa2d04f
alexlasota <alexylasota@gmail.com>
2024-10-31 10:22:55 +0100

    conference-service

---
+++ src/main/java/com/alexlasota/al_conference_service/AlConferenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.al_conference_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlConferenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlConferenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/al_conference_service/AlConferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.al_conference_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlConferenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
